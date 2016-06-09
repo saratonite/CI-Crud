b9a11428d021346233682978692887930652a81b
Sarath <sarath.hacks@gmail.com>
2016-06-09 15:32:25 +0530

    UI Improvements

--- application/views/user/login.php
+++ application/views/user/login.php
@@ -3,30 +3,32 @@
 
 
  <div class="login-panel">
-   <div class="panel panel-default">
+   <div class="panel panel-info">
 
+     <div class="panel-heading">
+       LOGIN
+     </div>
      <div class="panel-body">
-       login
-     </div>
-     <div class="panel-content">
+
+       <form class="" action="<?= base_url();?>index.php/user/proccess_login" method="post">
+
+         <div class="form-group">
+           <label for="">Email</label>
+           <input type="email" name="email" value="" class="form-control" placeholder="Email">
+         </div>
+         <div class="form-group">
+           <label for="">Password</label>
+           <input type="password" name="password" value="" class="form-control" placeholder="Password">
+         </div>
+         <div class="form-group">
+           <button type="submit" name="login" class="btn btn-info btn-block">Login</button>
+         </div>
+
+       </form>
 
      </div>
 
-     <form class="" action="<?= base_url();?>index.php/user/proccess_login" method="post">
 
-       <div class="form-group">
-         <label for="">Email</label>
-         <input type="email" name="email" value="" class="form-control" placeholder="Email">
-       </div>
-       <div class="form-group">
-         <label for="">Password</label>
-         <input type="password" name="password" value="" class="form-control" placeholder="Password">
-       </div>
-       <div class="form-group">
-         <button type="submit" name="login" class="btn btn-block">Login</button>
-       </div>
-
-     </form>
 
    </div>
 
